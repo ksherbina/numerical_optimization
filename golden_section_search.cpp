90031087886d1dc11ef646271e3020c970dcb13a
Katrina Sherbina <sherbina@usc.edu>
2017-09-11 15:21:14 -0700

    changed the while loop invariant

--- golden_section_search.cpp
+++ golden_section_search.cpp
@@ -33,7 +33,9 @@ valarray<double> golden_section_search(int n, double (*f)(valarray<double>), val
   int i=0;
   printf("%s %s %*s %*s %*s %*s %*s\n","Iteration","xs",s2,"xu",s2,"xl",s2,"xr",s2,"f(xl)",s2,"f(xr)");
   printf("%d %*.8f %*.8f %*.8f %*.8f %*.8f %*.8f \n",i,s1,xs[0],s2,xu[0],s2,xl[0],s2,xr[0],s2,fxl,s2,fxr);
-  while (i<10) {
+  //invariant: The euclidean norm of the difference between the left and right
+  //endpoints is greater than the stopping tolerance
+  while (diff>e) {
     if (fxr>fxl) {
       printf("fxr greate than fxl\n");
       xu=xr;
@@ -50,32 +52,9 @@ valarray<double> golden_section_search(int n, double (*f)(valarray<double>), val
       fxr=f(xr);
     }
     printf("%d %*.8f %*.8f %*.8f %*.8f %*.8f %*.8f \n",i,s1,xs[0],s2,xu[0],s2,xl[0],s2,xr[0],s2,fxl,s2,fxr);
+    diff=sqrt(pow(xu-xs, 2).sum());
+    printf("invariant: %2.8f \n",diff);
     i++;
   }
-  
-  //invariant: The euclidean norm of the difference between the left and right
-  //endpoints is greater than the stopping tolerance
-  //while (diff>e) {
-    //Since it is assumed that the direction has already been found, you are 
-    //only trying to find the step size. Therefore, you can just compare the 
-    //magnitude of the function evaluations.
-  //  fxl_len=sqrt(pow(fxl,2.0).sum());
-  //  printf("%2.8f \n",fxl_len);
-  //  fxr_len=sqrt(pow(fxr,2.0).sum());
-  //  if (fxr_len>fxl_len) {
-  //    xu=xr;
-  //    xr=xl;
-  //    fxr=f(xr);
-  //    xl=xs+(xu-xr);
-  //    fxl=f(xl);
-  //  } else {
-  //    xs=xl;
-  //    xl=xr;
-  //    fxl=fxr;
-  //    xr=xu-(xl-xs);
-  //    fxr=f(xr);
-  //  }
-  //  diff=sqrt(pow (xu-xs, 2).sum());
-  //}
   return (xu+xs)/2;
 }
