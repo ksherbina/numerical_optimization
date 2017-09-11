27f398227775d151515eee9515f58ab5f1267668
ksherbina <sherbina@usc.edu>
2017-09-11 13:01:10 -0700

    first attemp at full method

--- golden_section_search.cpp
+++ golden_section_search.cpp
@@ -4,13 +4,15 @@
 #include <cmath>
 #include <stdio.h> //include printf
 #include <valarray>
+#include <typeinfo>
 
 using std::valarray;
+using std::greater;
 
 valarray<double> golden_section_search(int n, valarray<double> (*f)(valarray<double>), valarray<double> x, double a, double b, valarray<double> d, double e)
 {
   valarray<double> xs (n), xu (n), xr (n), xl (n), fxr (n), fxl (n);
-  double diff;
+  double diff, fxl_len, fxr_len;
   double r=(0.5)*(1.0+sqrt (5.0));
   printf("golden ratio = %2.8f \n",r); 
   xs=x+a*d; //initialize left endpoint
@@ -27,5 +29,32 @@ valarray<double> golden_section_search(int n, valarray<double> (*f)(valarray<dou
   diff=sqrt(pow (xu-xs, 2).sum());  
   printf("invariant: %2.8f \n",diff);
 
-  return xs;
+  //invariant: The euclidean norm of the difference between the left and right 
+  //endpoints is greater than the stopping tolerance
+  //fxl_len=sqrt(pow(fxl,2.0).sum());
+  //printf("%2.8f \n",fxl_len);
+  //fxr_len=sqrt(pow(fxr,2.0).sum());
+  while (diff>e) {
+    //Since it is assumed that the direction has already been found, you are 
+    //only trying to find the step size. Therefore, you can just compare the 
+    //magnitude of the function evaluations.
+    fxl_len=sqrt(pow(fxl,2.0).sum());
+    printf("%2.8f \n",fxl_len);
+    fxr_len=sqrt(pow(fxr,2.0).sum());
+    if (fxr_len>fxl_len) {
+      xu=xr;
+      xr=xl;
+      fxr=f(xr);
+      xl=xs+(xu-xr);
+      fxl=f(xl);
+    } else {
+      xs=xl;
+      xl=xr;
+      fxl=fxr;
+      xr=xu-(xl-xs);
+      fxr=f(xr);
+    }
+    diff=sqrt(pow (xu-xs, 2).sum());
+  }
+  return (xu+xs)/2;
 }
