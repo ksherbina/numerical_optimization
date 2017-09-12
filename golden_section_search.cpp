eee62ac6ba148535e5c8d797957cfb148a6d2c1e
Katrina Sherbina <sherbina@usc.edu>
2017-09-11 17:36:21 -0700

    changed what is printed to stdout

--- golden_section_search.cpp
+++ golden_section_search.cpp
@@ -1,5 +1,5 @@
-//Implementation of Golden Section Search that can be used for multidimensional cases
-//to find a local minimum of a user-defined function.
+//Implementation of Golden Section Search to find a local minimum of a user-defined function
+//for both 1-dimensional and multidimensional inputs.
 #include <iostream>
 #include <cmath>
 #include <stdio.h> //include printf
@@ -7,44 +7,43 @@
 #include <typeinfo>
 
 using std::valarray;
-using std::greater;
 
 valarray<double> golden_section_search(int n, double (*f)(valarray<double>), valarray<double> x, double a, double b, valarray<double> d, double e)
 {
   //user-defined function f: R^n -> R.
   valarray<double> xs (n), xu (n), xr (n), xl (n);
   double diff, fxl, fxr;
+  //Since you want to stay within the initial left and right endpoints, use the solution to
+  //t^2+t-1=0 such that t>0 as the golden ratio r.
   double r=(0.5)*(sqrt(5.0)-1);
-  printf("golden ratio = %2.8f \n",r); 
+  //printf("golden ratio = %2.8f \n",r);
   xs=x+a*d; //initialize left endpoint
   xu=x+b*d; //initialize right endpoint
-  std::cout<<"size of xs: "<<xs.size()<<" size of xu: "<<xu.size()<<std::endl;
-  printf("xs: %2.8f, xu: %2.8f \n",xs[0],xu[0]);
-  xr=xs+r*(b-a)*d;
-  xl=xs+(1.0-r)*(b-a)*d;
+  xr=xs+r*(b-a)*d; //initialize the right-hand search point
+  xl=xs+(1.0-r)*(b-a)*d; //initialize the left-hand search point
   fxr=f(xr);
   fxl=f(xl);
-  printf("xr = %2.8f, f(xr) = %2.8f \n",xr[0],fxr);
-  printf("xl = %2.8f, f(xl) = %2.8f \n",xl[0],fxl);
   diff=sqrt(pow(xu-xs, 2).sum());
-  printf("invariant: %2.8f \n",diff);
   
-  int s1=18,s2=15;
+  int s1=18,s2=16;
   int i=0;
-  printf("%s %s %*s %*s %*s %*s %*s\n","Iteration","xs",s2,"xu",s2,"xl",s2,"xr",s2,"f(xl)",s2,"f(xr)");
+  printf("***NOTE: xs=left endpoint; xu=right endpoint; xl=left-hand search point; xr=right-hand search point***\n");
+  printf("%s %s %*s %*s %*s %*s %*s\n","Iteration","xs",s2,"xu",s2,"xl",s2,"xr",s1,"f(xl)",s2,"f(xr)");
   printf("%d %*.8f %*.8f %*.8f %*.8f %*.8f %*.8f \n",i,s1,xs[0],s2,xu[0],s2,xl[0],s2,xr[0],s2,fxl,s2,fxr);
+  
   //invariant: The euclidean norm of the difference between the left and right
   //endpoints is greater than the stopping tolerance
   while (diff>e) {
+    i++;
     if (fxr>fxl) {
-      printf("fxr greate than fxl\n");
+      //printf("fxr greate than fxl\n");
       xu=xr;
       xr=xl;
       fxr=fxl;
       xl=xs+(xu-xr);
       fxl=f(xl);
     } else {
-      printf("fxr less than or equal to fxl\n");
+      //printf("fxr less than or equal to fxl\n");
       xs=xl;
       xl=xr;
       fxl=fxr;
@@ -53,8 +52,7 @@ valarray<double> golden_section_search(int n, double (*f)(valarray<double>), val
     }
     printf("%d %*.8f %*.8f %*.8f %*.8f %*.8f %*.8f \n",i,s1,xs[0],s2,xu[0],s2,xl[0],s2,xr[0],s2,fxl,s2,fxr);
     diff=sqrt(pow(xu-xs, 2).sum());
-    printf("invariant: %2.8f \n",diff);
-    i++;
+    //printf("invariant: %2.8f \n",diff);
   }
   return (xu+xs)/2;
 }
--- hw1_2a.cpp
+++ hw1_2a.cpp
@@ -1,4 +1,4 @@
-//Function for problem 2a of homework 1 in ISE 520 Fall 2017
+//Function, inputs, and parameters for problem 2a of homework 1 in ISE 520 Fall 2017
 #include <iostream>
 #include <cmath>
 #include <stdio.h> //include printf
@@ -17,22 +17,18 @@ int main()
 {
   clock_t t;
   t=clock();
-  int n=1;
+  int n=1; //User must specify number of dimensions
   valarray<double> d (n), x (n), xs (n), soln(n);
-  double a,b,error,fval1;
-
+  double a,b,error;
+  
+  //User specified inputs and parameters:
   d[0]=1.0;
   x[0]=0.0;
   a=0;
   b=3;
   error=pow(10.0,-4);
-  printf("Interval of Uncertainity = %2.8f \n",error);
-  //fval1=func(x+b*d);
-  //printf("x = %2.8f \n",(x+b*d)[0]);
-  //printf("f(x) = %2.8f \n",fval1);
   soln=golden_section_search(n,func,x,a,b,d,error);
-  std::cout<<"size of soln: "<<soln.size()<<std::endl;
-  printf("soln: %2.8f \n",soln[0]);
+  printf("Local minimum = %2.8f (given interval of uncertainty of %2.8f)\n",soln[0],error);
   t=clock()-t;
   printf ("Runtime of algorithm: %ld clicks (%f seconds).\n",t,((float)t)/CLOCKS_PER_SEC);
   return 0;
