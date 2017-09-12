943bf03091759d7be968246abc14a527d994d900
Katrina Sherbina <sherbina@usc.edu>
2017-09-11 19:09:05 -0700

    initialization of dichotomous search

--- dichotomous_search.cpp
+++ dichotomous_search.cpp
@@ -1,4 +1,4 @@
-//Implementation of Golden Section Search to find a local minimum of a user-defined function
+//Implementation of Dichotomous Search to find a local minimum of a user-defined function
 //for both 1-dimensional and multidimensional inputs.
 #include <iostream>
 #include <cmath>
@@ -8,19 +8,23 @@
 
 using std::valarray;
 
-valarray<double> golden_section_search(int n, double (*f)(valarray<double>), valarray<double> x, double a, double b, valarray<double> d, double e)
+valarray<double> dichotomous_search(int n, double (*f)(valarray<double>), valarray<double> x, double a, double b, valarray<double> d, double epsilon, double eta)
 {
   //User must supply the function f: R^n -> R.
   valarray<double> xs (n), xu (n), xr (n), xl (n);
   double diff, fxl, fxr;
-  //Since you want to stay within the initial left and right endpoints, use the solution to
-  //t^2+t-1=0 such that t>0 as the golden ratio r.
-  double r=(0.5)*(sqrt(5.0)-1);
-  //printf("golden ratio = %2.8f \n",r);
+  //delta is the distinguishability constant such that eta = 2*delta > 0
+  printf("Distinguishability constant delta such that %2.8f = 2*delta > 0 :\n",eta);
+  valarray<double> delta (eta/2.0, n);
+  for (int i=0; i<delta.size(); i++) {
+    std::cout<<delta[i]<<' ';
+  }
+  std::cout<<std::endl;
+  
   xs=x+a*d; //initialize left endpoint
   xu=x+b*d; //initialize right endpoint
-  xr=xs+r*(b-a)*d; //initialize the right-hand search point
-  xl=xs+(1.0-r)*(b-a)*d; //initialize the left-hand search point
+  xl=(0.5)*(a+b)*d-delta; //initialize the left-hand search point
+  xr=(0.5)*(a+b)*d+delta; //initialize the right-hand search point
   fxr=f(xr);
   fxl=f(xl);
   diff=sqrt(pow(xu-xs, 2).sum());
@@ -33,26 +37,26 @@ valarray<double> golden_section_search(int n, double (*f)(valarray<double>), val
   
   //invariant: The euclidean norm of the difference between the left and right
   //endpoints is greater than the stopping tolerance
-  while (diff>e) {
-    i++;
-    if (fxr>fxl) {
-      //printf("fxr greate than fxl\n");
-      xu=xr;
-      xr=xl;
-      fxr=fxl;
-      xl=xs+(xu-xr);
-      fxl=f(xl);
-    } else {
-      //printf("fxr less than or equal to fxl\n");
-      xs=xl;
-      xl=xr;
-      fxl=fxr;
-      xr=xu-(xl-xs);
-      fxr=f(xr);
-    }
-    printf("%d %*.8f %*.8f %*.8f %*.8f %*.8f %*.8f \n",i,s1,xs[0],s2,xu[0],s2,xl[0],s2,xr[0],s2,fxl,s2,fxr);
-    diff=sqrt(pow(xu-xs, 2).sum());
+  //while (diff>epsilon) {
+  //  i++;
+  //  if (fxr>fxl) {
+     //printf("fxr greate than fxl\n");
+  //    xu=xr;
+  //    xr=xl;
+  //    fxr=fxl;
+  //    xl=xs+(xu-xr);
+  //    fxl=f(xl);
+  //  } else {
+  //    //printf("fxr less than or equal to fxl\n");
+  //    xs=xl;
+  //    xl=xr;
+  //    fxl=fxr;
+  //    xr=xu-(xl-xs);
+  //    fxr=f(xr);
+  //  }
+  //  printf("%d %*.8f %*.8f %*.8f %*.8f %*.8f %*.8f \n",i,s1,xs[0],s2,xu[0],s2,xl[0],s2,xr[0],s2,fxl,s2,fxr);
+  //  diff=sqrt(pow(xu-xs, 2).sum());
     //printf("invariant: %2.8f \n",diff);
-  }
+  //}
   return (xu+xs)/2;
 }
