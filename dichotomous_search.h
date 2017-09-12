1c9042a326f23ed2779806fe36a6641883ed3746
Katrina Sherbina <sherbina@usc.edu>
2017-09-11 19:06:23 -0700

    header file for dichotomous search function

--- dichotomous_search.h
+++ dichotomous_search.h
@@ -1,12 +1,12 @@
-// This is start of the header guard.  ADD_H can be any unique name.  By convention, we use the name of the header file.
-#ifndef GOLDEN_SECTION_SEARCH_H
-#define GOLDEN_SECTION_SEARCH_H
+// This is start of the header guard.  <>_H can be any unique name.  By convention, we use the name of the header file.
+#ifndef DICHOTOMOUS_SEARCH_H
+#define DICHOTOMOUS_SEARCH_H
  
 // This is the content of the .h file, which is where the declarations go
 #include <valarray>
 using std::valarray;
 
-valarray<double> golden_section_search(int n, double (*f)(valarray<double>), valarray<double> x, double a, double b, valarray<double> d, double e);
+valarray<double> dichotomous_search(int n, double (*f)(valarray<double>), valarray<double> x, double a, double b, valarray<double> d, double epsilon, double eta);
  
 // This is the end of the header guard
 #endif
