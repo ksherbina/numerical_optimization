60298fd562d8cae0c5710326e272a52d12216fb0
Katrina Sherbina <sherbina@usc.edu>
2017-09-11 21:15:52 -0700

    added the rest of the algorithm

--- dichotomous_search.cpp
+++ dichotomous_search.cpp
@@ -12,7 +12,7 @@ valarray<double> dichotomous_search(int n, double (*f)(valarray<double>), valarr
 {
   //User must supply the function f: R^n -> R.
   valarray<double> xs (n), xu (n), xr (n), xl (n);
-  double diff, fxl, fxr;
+  double diff, diff0, fxl, fxr;
   //delta is the distinguishability constant such that eta = 2*delta > 0
   printf("Distinguishability constant delta such that %2.8f = 2*delta > 0 :\n",eta);
   valarray<double> delta (eta/2.0, n);
@@ -23,8 +23,8 @@ valarray<double> dichotomous_search(int n, double (*f)(valarray<double>), valarr
   
   xs=x+a*d; //initialize left endpoint
   xu=x+b*d; //initialize right endpoint
-  xl=(0.5)*(a+b)*d-delta; //initialize the left-hand search point
-  xr=(0.5)*(a+b)*d+delta; //initialize the right-hand search point
+  xl=0.5*(a+b)*d-delta; //initialize the left-hand search point
+  xr=0.5*(a+b)*d+delta; //initialize the right-hand search point
   fxr=f(xr);
   fxl=f(xl);
   diff=sqrt(pow(xu-xs, 2).sum());
@@ -37,26 +37,27 @@ valarray<double> dichotomous_search(int n, double (*f)(valarray<double>), valarr
   
   //invariant: The euclidean norm of the difference between the left and right
   //endpoints is greater than the stopping tolerance
-  //while (diff>epsilon) {
-  //  i++;
-  //  if (fxr>fxl) {
-     //printf("fxr greate than fxl\n");
-  //    xu=xr;
-  //    xr=xl;
-  //    fxr=fxl;
-  //    xl=xs+(xu-xr);
-  //    fxl=f(xl);
-  //  } else {
-  //    //printf("fxr less than or equal to fxl\n");
-  //    xs=xl;
-  //    xl=xr;
-  //    fxl=fxr;
-  //    xr=xu-(xl-xs);
-  //    fxr=f(xr);
-  //  }
-  //  printf("%d %*.8f %*.8f %*.8f %*.8f %*.8f %*.8f \n",i,s1,xs[0],s2,xu[0],s2,xl[0],s2,xr[0],s2,fxl,s2,fxr);
-  //  diff=sqrt(pow(xu-xs, 2).sum());
-    //printf("invariant: %2.8f \n",diff);
-  //}
+  while (diff>epsilon) {
+    diff0=diff;
+    i++;
+    if (fxr>fxl) {
+      //printf("fxr greate than fxl\n");
+      xu=xr;
+    } else {
+      //printf("fxr less than or equal to fxl\n");
+      xs=xl;
+    }
+    xl=0.5*(xs+xu)*d-delta;
+    xr=0.5*(xs+xu)*d+delta;
+    fxl=f(xl);
+    fxr=f(xr);
+    printf("%d %*.8f %*.8f %*.8f %*.8f %*.8f %*.8f \n",i,s1,xs[0],s2,xu[0],s2,xl[0],s2,xr[0],s2,fxl,s2,fxr);
+    diff=sqrt(pow(xu-xs, 2).sum());
+    if (diff0==diff) {
+      printf("Cannot find a minimum given the interval of uncertainty of %2.8f.\n",epsilon);
+      printf("||xu-xs|| = %2.8f\n",diff);
+      break;
+    }
+  }
   return (xu+xs)/2;
 }
