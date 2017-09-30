acbc9035d68e22f7533cb7dde4841dd6363e8b10
Katrina Sherbina <sherbina@usc.edu>
2017-09-29 17:48:07 -0700

    added more of algorithm

--- armijo_rule.cpp
+++ armijo_rule.cpp
@@ -5,17 +5,25 @@ to minimize a function.
 */
 #include <iostream>
 #include <cmath>
-#include <stdio.h> //include printfin
+#include <stdio.h> //for printf
 #include <valarray>
-#include <typeinfo>
+#include <numeric> //for std::inner_product
 
 using std::valarray;
 
-double armijo_rule(int n,double (*f)(valarray<double>),valarray<double> (*g)(valarray<double>),valarray<double> xk, valarray<double> d, double a, double eta, double theta)
+double armijo_rule(int n,double (*f)(valarray<double>),valarray<double> gxk,valarray<double> xk, valarray<double> d, double a, double eta, double theta)
 {
   //User must supply the function f: R^n -> R and the gradient of the function g:R^n.
-  double alpha;
+  valarray<double> xc(n);
+  double alpha,fxc,lc,prod;
+  
+  int t=1;
   alpha=a;
+  double fxk=f(xk);
+  xc=xk+alpha*d;
+  fxc=f(xc);
+  prod=std::inner_product(std::begin(gxk),std::end(gxk),std::begin(d),0.0);
+  std::cout<<prod<<std::endl;
  
   return alpha;
 }
