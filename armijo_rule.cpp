69afda5769868f4b445c220b832b6b49b6bdad52
Katrina Sherbina <sherbina@usc.edu>
2017-09-29 23:03:28 -0700

    fixed input arguments and algorithm

--- armijo_rule.cpp
+++ armijo_rule.cpp
@@ -14,55 +14,57 @@ to minimize a function.
 
 using std::valarray;
 
-int comparison (int iter,double (*f)(valarray<double>),valarray<double> x0,double fx0,valarray<double> gx0,valarray<double> p,double multiplier,double th,valarray<double>& xc)
+int comparison (int iter,double (*f)(valarray<double>),valarray<double> (*g)(valarray<double>), valarray<double>& xk,valarray<double> p,double multiplier,double th)
 {
-  double fxc,lc,prod;
-  xc=x0+(multiplier*p);
+  valarray<double> xc,gxc;
+  double fxc,fxk,lc,prod;
+  xc=xk;
   fxc=f(xc);
-  prod=std::inner_product(std::begin(gx0),std::end(gx0),std::begin(p),0.0);
-  lc=fx0+multiplier*th*prod;
+  gxc=g(xc);
+  prod=std::inner_product(std::begin(gxc),std::end(gxc),std::begin(p),0.0);
+  lc=fxc+multiplier*th*prod;
+  xk=xc+(multiplier*p);
+  fxk=f(xk);
 
-  int s1=22,s2=16;
+  int s1=12;
   //printf("%d & %*.8f & %*.8f & %*.8f & %*.8f & %*.8f & %*.8f \\\\ \n",i,s1,xs[0],s2,xu[0],s2,xl[0],s2,xr[0],s2,fxl,s2,fxr);
-  printf("%d %*.8f %*.8f %*.8f %*.8f %*.8f\n",iter,s1,multiplier,s2,xc[0],s2,xc[1],s2,fxc,s2,lc);
+  printf("%d \t               [%.8f %.8f]    [%.8f %.8f]    %*.8f \t %*.8f\n",iter,xc[0],xc[1],xk[0],xk[1],s1,fxc,s1,lc);
   
-  if (fxc<=lc) {
+  if (fxk<=lc) {
     return 1;
   } else {
     return 0;
   }
 }
 
-valarray<double> armijo_rule(int n,double (*f)(valarray<double>),valarray<double> gxk,valarray<double> xk,valarray<double> d, double a, double eta, double theta)
+valarray<double> armijo_rule(int n,double (*f)(valarray<double>),valarray<double> (*g)(valarray<double>),valarray<double> x0,valarray<double> d, double a, double eta, double theta)
 {
   //User must supply the function f: R^n -> R and the gradient of the function g:R^n.
   double alpha;
   int t=1;
   alpha=a;
-  double fxk=f(xk);
-  valarray<double> xc=xk;
+  valarray<double> xc=x0;
   
   std::cout<<std::endl;
-  printf("NOTE 1: multiplier = (eta^j)^i*alpha where j is either 1 or -1 depending on whether\n");
-  printf("alpha is doubled or halved in that iteration i.\n");
-  printf("NOTE 2: r(multiplier) = f(x_0)+(multiplier*theta*grad*d) where grad is the gradient\n");
-  printf("of f at the initial point x_0 and d is the user-supplied direction.\n");
+  printf("NOTE 1: x_i = x_{i-1}+(eta^j)^i*alpha*grad where j is either 1 or -1 depending on whether\n");
+  printf("alpha is doubled or halved in that iteration i and grad is the gradient of f at x_{i-1}.\n");
+  printf("NOTE 2: r(i) = f(x_{i-1})+(eta^j)^i*alpha*theta*grad*d\n");
   
   std::cout<<std::endl;
-  int s1=22,s2=16;
-  printf("%s %s %*s %*s %*s %*s\n","Iteration (i)","multiplier",s2,"x_i",s2,"y_i",s2,"f(x_i,y_i)",s2,"r(multiplier)");
+  int s1=30,s2=16,s3=22;
+  printf("%s %*s %*s %*s %*s\n","Iteration (i)",s2,"x_{i-1}",s3,"x_i",s1,"f(x_i)",s3,"r(x_i)");
   
-  if (comparison(t,f,xk,fxk,gxk,d,alpha,theta,xc)==1) {
+  if (comparison(t,f,g,xc,d,alpha,theta)==1) {
     //std::cout<<"f(x_i) <= r(multiplier)"<<std::endl;
     alpha=pow(eta,t)*alpha;
-    while (comparison(t,f,xk,fxk,gxk,d,alpha,theta,xc)==0) {
+    while (comparison(t,f,g,xc,d,alpha,theta)==0) {
       t+=1;
       alpha=pow(eta,t)*alpha;
     }
   } else {
     //std::cout<<"f(x_i) > r(multiplier)"<<std::endl;
     alpha=pow(1/eta,t)*alpha;
-    while (comparison(t,f,xk,fxk,gxk,d,alpha,theta,xc)==0) {
+    while (comparison(t,f,g,xc,d,alpha,theta)==0) {
       t+=1;
       alpha=pow(1/eta,t)*alpha;
     }
