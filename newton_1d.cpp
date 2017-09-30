e6f3ac3d50f1cbff9f96e49761e5fa937a3392af
Katrina Sherbina <sherbina@usc.edu>
2017-09-29 21:25:15 -0700

    changed the entire algorithm to newton's method

--- newton_1d.cpp
+++ newton_1d.cpp
@@ -4,63 +4,50 @@
 #include <cmath>
 #include <stdio.h> //include printf
 #include <valarray>
-#include <typeinfo>
+#include <tuple>
 
 using std::valarray;
 
-valarray<double> dichotomous_search(int n, double (*f)(valarray<double>), valarray<double> x, double a, double b, valarray<double> d, double epsilon, double eta)
+std::tuple<double, double>  newton_1d(double (*f)(double),double (*g)(double),double (*h)(double),double x0, double a, double b, double epsilon, double theta)
 {
-  //User must supply the function f: R^n -> R.
-  valarray<double> xs (n), xu (n), xr (n), xl (n);
-  double diff, diff0, fxl, fxr;
-  //delta is the distinguishability constant such that eta = 2*delta > 0
-  printf("Distinguishability constant delta such that %2.8f = 2*delta > 0 :\n",eta);
-  valarray<double> delta (eta/2.0, n);
-  for (int i=0; i<delta.size(); i++) {
-    std::cout<<delta[i]<<' ';
-  }
-  std::cout<<std::endl;
+  //User must supply the functions f: R^n -> R, the gradient of f g:R^n, and hessian of f h:R^n.
+  double d,alpha,xd,fxd;
+  double xc=x0;
+  double fxc=f(xc);
+  double gxc=g(xc);
+  double hxc=h(xc);
   
-  xs=x+a*d; //initialize left endpoint
-  xu=x+b*d; //initialize right endpoint
-  xl=0.5*(a+b)*d-delta; //initialize the left-hand search point
-  xr=0.5*(a+b)*d+delta; //initialize the right-hand search point
-  fxr=f(xr);
-  fxl=f(xl);
-  diff=sqrt(pow(xu-xs, 2).sum());
-  
+  /*
   int s1=18,s2=16;
-  int i=0;
-  printf("***NOTE: xs=left endpoint; xu=right endpoint; xl=left-hand search point; xr=right-hand search point***\n");
   //printf("%s & %s & %*s & %*s & %*s & %*s & %*s \\\\ \n","Iteration","xs",s2,"xu",s2,"xl",s2,"xr",s1,"f(xl)",s2,"f(xr)");
   //printf("%d & %*.8f & %*.8f & %*.8f & %*.8f & %*.8f & %*.8f \\\\ \n",i,s1,xs[0],s2,xu[0],s2,xl[0],s2,xr[0],s2,fxl,s2,fxr);
   printf("%s %s %*s %*s %*s %*s %*s\n","Iteration","xs",s2,"xu",s2,"xl",s2,"xr",s1,"f(xl)",s2,"f(xr)");
   printf("%d %*.8f %*.8f %*.8f %*.8f %*.8f %*.8f \n",i,s1,xs[0],s2,xu[0],s2,xl[0],s2,xr[0],s2,fxl,s2,fxr);
-         
-  //invariant: The euclidean norm of the difference between the left and right
-  //endpoints is greater than the stopping tolerance
-  while (diff>epsilon) {
-    diff0=diff;
-    i++;
-    if (fxr>fxl) {
-      //printf("fxr greate than fxl\n");
-      xu=xr;
+   */
+  
+  while (std::abs(gxc)>epsilon) {
+    if (hxc>0.0) {
+      d=-gxc/hxc;
     } else {
-      //printf("fxr less than or equal to fxl\n");
-      xs=xl;
+      d=-gxc;
     }
-    xl=0.5*(xs+xu)*d-delta;
-    xr=0.5*(xs+xu)*d+delta;
-    fxl=f(xl);
-    fxr=f(xr);
-    //printf("%d & %*.8f & %*.8f & %*.8f & %*.8f & %*.8f & %*.8f \\\\ \n",i,s1,xs[0],s2,xu[0],s2,xl[0],s2,xr[0],s2,fxl,s2,fxr);
-    printf("%d %*.8f %*.8f %*.8f %*.8f %*.8f %*.8f \n",i,s1,xs[0],s2,xu[0],s2,xl[0],s2,xr[0],s2,fxl,s2,fxr);
-    diff=sqrt(pow(xu-xs, 2).sum());
-    if (diff0==diff) {
-      printf("Cannot find a minimum given the interval of uncertainty of %2.8f.\n",epsilon);
-      printf("||xu-xs|| = %2.8f\n",diff);
-      break;
+    if (d<0.0) {
+      alpha=std::min(1.0,(a-xc)/d);
     }
+    if (d>0.0) {
+      alpha=std::min(1.0,(b-xc)/d);
+    }
+    xd=xc+alpha*d;
+    fxd=f(xd);
+    while (fxd>=fxc) {
+      alpha=theta*alpha;
+      xd=xc+alpha*d;
+      fxd=f(xd);
+    }
+    xc=xd;
+    fxc=fxd;
+    gxc=g(xc);
+    hxc=h(xc);
   }
-  return (xu+xs)/2;
+  return std::make_tuple(xc,fxc);
 }
---
+++ newton_1d.h
@@ -0,0 +1,9 @@
+#ifndef ARMIJO_RULE_H
+#define ARMIJO_RULE_H
+ 
+#include <valarray>
+using std::valarray;
+
+valarray<double> armijo_rule(int n,double (*f)(valarray<double>),valarray<double> g,valarray<double> xk, valarray<double> d, double a, double eta, double theta);
+ 
+#endif
