dfa4de3215ab9a549832dc8d7f26f4dec894d111
Katrina Sherbina <sherbina@usc.edu>
2017-10-27 18:44:44 -0700

    added calls to more functions

--- hw3.cpp
+++ hw3.cpp
@@ -13,7 +13,16 @@
 #include "solve_linear_system.h"
 
 int main() {
+  int ncol = 3;
+  CholeskyFactors check_chol;
+  valarray<double> check_solv;
+  valarray<double> M = { 2.0, -1.0, 1.0, -1.0, 3.0, 0.0, 1.0, 0.0, 5.0 };
+  valarray<double> b = { 1.0, -2.0, 3.0 };
+  
+  check_chol = cholesky(M, ncol, 0);
+  check_solv = solve_linear_system(check_chol, b, ncol);
   std::cout<<std::numeric_limits<double>::epsilon()<<std::endl;
+  
   return 0;
 }
 
