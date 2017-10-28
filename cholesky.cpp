715d912ce926cedfee178be4159c4be5993945b4
Katrina Sherbina <sherbina@usc.edu>
2017-10-27 18:37:51 -0700

    minor changes

--- cholesky.cpp
+++ cholesky.cpp
@@ -19,60 +19,59 @@ struct CholeskyFactors {
     valarray<double> diagonal;
 };
 
-CholeskyFactors cholesky(valarray<double> A, int n, int fix)
-{
+CholeskyFactors cholesky(valarray<double> A, int n, int fix) {
   /* The fix parameter is set to 1 to ensure that the diagonal entries D
   are sufficiently positive. Otherwise, D may not be positive definite.
   */
   CholeskyFactors result;
   valarray<double> C (0.0,n*n), D (0.0,n*n), L (0.0,n*n);
-  double sum1,sum2,theta;
-  double delta=pow(10.0,-8);
-  double beta=2.0;
+  double sum1 ,sum2, theta;
+  double delta = pow(10.0, -8);
+  double beta = 2.0;
 
-  for (int k=0; k<n; k++) {
-    L[k*n+k]=1.0;
+  for (int k = 0; k < n; k++) {
+    L[k * n + k] = 1.0;
   }
-  if (fix==1) {
-    theta=*std::max_element(std::begin(C)+1,std::end(C));
-    double e[3]={C[0],(theta/beta)*(theta/beta),delta};
-    D[0]=*std::max_element(std::begin(e),std::end(e));
+  if (fix == 1) {
+    theta = *std::max_element(std::begin(C) + 1, std::end(C));
+    double e[3] = {C[0], (theta / beta) * (theta / beta), delta};
+    D[0] = *std::max_element(std::begin(e), std::end(e));
   } else {
-    D[0]=A[0];
+    D[0] = A[0];
   }
-  for (int k=1; k<n; k++) {
-    L[k*n]=A[k*n]/D[0];
+  for (int k = 1; k < n; k++) {
+    L[k * n] = A[k * n] / D[0];
   }
-  for (int j=1; j<n; j++) {
-    sum1=0.0;
-    for (int s=0; s<j; s++) {
-      sum1=sum1+D[s]*(L[j*n+s]*L[j*n+s]);
+  for (int j =1 ; j < n; j++) {
+    sum1 = 0.0;
+    for (int s = 0; s < j; s++) {
+      sum1 = sum1 + D[s] * (L[j * n + s] * L[j * n + s]);
     } 
-    C[j*n+j]=A[j*n+j]-sum1;
-    if (fix==1) {
-    theta=*std::max_element(std::begin(C)+(j+1),std::end(C));
-    double e[3]={C[0],(theta/beta)*(theta/beta),delta};
-    D[0]=*std::max_element(std::begin(e),std::end(e));
+    C[j * n + j] = A[j * n + j] - sum1;
+    if (fix == 1) {
+    theta =*std::max_element(std::begin(C) + (j + 1), std::end(C));
+    double e[3] = {C[0], (theta / beta) * (theta / beta), delta};
+    D[0] =*std::max_element(std::begin(e), std::end(e));
     } else {
-      D[j]=C[j*n+j];
+      D[j] = C[j * n + j];
     }
-    for (int i=j+1; i<n; i++) {
-      sum2=0.0;
-      for (int t=0; t<j; t++) {
-        sum2=sum2+D[t]*L[i*n+t]*L[j*n+t];
+    for (int i = j + 1; i < n; i++) {
+      sum2 = 0.0;
+      for (int t = 0; t < j; t++) {
+        sum2 = sum2 + D[t] * L[i * n + t] * L[j * n + t];
       }
-      C[i*n+j]=A[i*n+j]-sum2;
-      L[i*n+j]=C[i*n+j]/D[j];
+      C[i * n + j] = A[i * n + j] - sum2;
+      L[i * n + j] = C[i * n + j] / D[j];
     }
   }
 
-  result.lower_triangular=L;
-  result.diagonal=D;
-  for (int i=0;i<n*n;i++) {
-      printf("lower triangular=%2.8f\n",result.lower_triangular[i]);
+  result.lower_triangular = L;
+  result.diagonal = D;
+  for (int i = 0; i < n * n; i++) {
+      printf("lower triangular=%2.8f\n", result.lower_triangular[i]);
   }
-  for (int i=0;i<n;i++) {
-      printf("diagonal=%2.8f\n",result.diagonal[i]);
+  for (int i = 0; i < n; i++) {
+      printf("diagonal=%2.8f\n", result.diagonal[i]);
   }
   return result;
 }
--- solve_linear_system.cpp
+++ solve_linear_system.cpp
@@ -16,34 +16,30 @@ be symmetric, positive definite).
 
 using std::valarray;
 
-valarray<double> solve_linear_system(CholeskyFactors factors, valarray<double> b, int n)
-{
-  valarray<double> x (0.0,n), y (0.0,n), p (0.0,n);
-  double backsum,forwardsum;
-  x[0]=b[0];
-  for (int i=1; i<n; i++) {
-    backsum=0.0;
-    for (int k=i-1; k>=0; k--) {
-      backsum=backsum+factors.lower_triangular[i*n+k]*x[k];
+valarray<double> solve_linear_system(CholeskyFactors factors, valarray<double> b, int n) {
+  valarray<double> x(0.0,n), y(0.0,n), p(0.0,n);
+  double backsum, forwardsum;
+  x[0] = b[0];
+  for (int i = 1; i < n; i++) {
+    backsum = 0.0;
+    for (int k = i - 1; k >= 0; k--) {
+      backsum = backsum + factors.lower_triangular[i * n + k] * x[k];
     }
-    x[i]=b[i]-backsum;
+    x[i] = b[i] - backsum;
   }
-  for (int i=0; i<n; i++) {
-    y[i]=x[i]/factors.diagonal[i];
+  for (int i = 0; i < n; i++) {
+    y[i] = x[i] / factors.diagonal[i];
   }
-  p[n-1]=y[n-1];
-  for (int i=n-2; i>=0; i--) {
-    forwardsum=0.0;
-    for (int j=i+1; j<n; j++) {
-      forwardsum=forwardsum+factors.lower_triangular[j*n+i]*p[j];
+  p[n - 1] = y[n - 1];
+  for (int i = n - 2; i >= 0; i--) {
+    forwardsum = 0.0;
+    for (int j = i + 1; j < n; j++) {
+      forwardsum = forwardsum + factors.lower_triangular[j * n + i] * p[j];
     }
-    p[i]=y[i]-forwardsum;
+    p[i] = y[i] - forwardsum;
   }
-  for (int i=0;i<n;i++) {
-      printf("x=%2.8f\n",x[i]);
-  }
-  for (int i=0;i<n;i++) {
-      printf("y=%2.8f\n",y[i]);
+  for (int k = 0; k < n; k++) {
+    std::cout<<"p["<<k<<"]="<<p[k]<<std::endl;
   }
   return p;
 }
