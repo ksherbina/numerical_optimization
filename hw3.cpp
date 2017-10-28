91a3065e90789d5efaba73b8dae5f3b3a9f89f5d
Katrina Sherbina <sherbina@usc.edu>
2017-10-27 17:20:51 -0700

    first commit

---
+++ hw3.cpp
@@ -0,0 +1,19 @@
+/*
+ Solutions to Homework 3 from ISE 520 Fall 2017 by Katrina Sherbina.
+ */
+#include <iostream>
+#include <cmath>
+#include <stdio.h> //for printf
+#include <valarray>
+#include <numeric> //for std::inner_product
+#include <string>
+#include <iostream>
+#include <iterator>
+#include "cholesky.h"
+#include "solve_linear_system.h"
+
+int main() {
+  std::cout<<std::numeric_limits<double>::epsilon()<<std::endl;
+  return 0;
+}
+
