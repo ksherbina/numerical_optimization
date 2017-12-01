3744df701e6af114ab0edecea8f6f8ec1363a006
Katrina Sherbina <sherbina@usc.edu>
2017-12-01 13:32:18 -0800

    deleted files

--- newton_1d.h
+++
@@ -1,8 +0,0 @@
-#ifndef NEWTON_1D_H
-#define NEWTON_1D_H
- 
-#include <tuple>
-
-std::tuple<double, double>  newton_1d(double (*f)(double),double (*g)(double),double (*h)(double),double x0, double a, double b, double epsilon, double theta);
- 
-#endif
