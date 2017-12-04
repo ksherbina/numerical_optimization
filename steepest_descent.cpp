a96691f6352ccc72cd2eb7c41f76f7b40c5304bb
Katrina Sherbina <sherbina@usc.edu>
2017-12-04 12:56:25 -0800

    fixed print statements

--- steepest_descent.cpp
+++ steepest_descent.cpp
@@ -15,7 +15,7 @@ valarray<double> steepest_descent(double (*f)(valarray<double>), valarray<double
 
   int k = 0;  
   int func_evals = 0;
-  double stepsize, gnorm, a, b;
+  double stepsize, gnorm, a, b, fx;
   valarray<double> x, gx, d, newx;
   Armijo steps;
   
@@ -25,16 +25,19 @@ valarray<double> steepest_descent(double (*f)(valarray<double>), valarray<double
   
   int gradient_evaluations = 1;
 
+  /*
   std::cout << "Enter left endpoint for Golden Section Search: ";
   std::cin >> a;
   std::cout << "Enter right endpoint for Golden Section Search: ";
   std::cin >> b;
+  */
 
   int s1=22,s2=16,s3=14;
   printf("%s %s %*s %*s %*s \n","Iteration (i)", "x_i[0]", s2, "x_i[1]", s2, "f(x_i)", s2, "norm of gradient");
 
   while (gnorm > epsilon) {
-    printf("%d %*.8f %*.8f %*.8f \n", k, s1, x[0], s3, x[1], s3, gnorm);
+    fx = f(x);
+    printf("%d %*.8f %*.8f %*.8f %*.8f \n", k, s1, x[0], s3, x[1], s3, fx, s3, gnorm);
     d = -gx;
     steps = armijo_rule(f, gx, x, d, 2.0, 0.5);
     func_evals += steps.fevals;
@@ -46,10 +49,11 @@ valarray<double> steepest_descent(double (*f)(valarray<double>), valarray<double
     gnorm = std::sqrt(std::inner_product(std::begin(gx), std::end(gx), std::begin(gx), 0.0));
     k++;
     if (k > MAX_ITER) {
-      printf("Maximum iterations exceeded");
+      printf("Maximum iterations exceeded \n");
       break;
     }
   }
+  std::cout<<func_evals<<" function evaluations"<<std::endl;
   std::cout<<gradient_evaluations<<" gradient evaluations"<<std::endl;
   return x;
 }
